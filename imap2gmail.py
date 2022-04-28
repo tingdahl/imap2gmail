fa815c118e9e3b73368163f146320b8f0c434369
Kristofer Tingdahl <10433010+tingdahl@users.noreply.github.com>
2022-04-28 10:14:48 +0200

    Renamed files.

--- imap2gmail.py
+++ imap2gmail.py
@@ -1,56 +1,26 @@
-from __future__ import print_function
+# List number of messages in INBOX folder
+# and print details of the messages that are not deleted
 
-import os.path
+import imaptraverser
+import logging
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+logging.basicConfig(level=logging.INFO )
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+imapcredentials = imaptraverser.ImapCredentials( "imap_credentials.json")
+if imapcredentials.isOK()==False:
+    logging.error("Credentials not read")
+    exit(1)
 
-
-def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    creds = None
-    # The file gmail_token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('gmail_token.json'):
-        creds = Credentials.from_authorized_user_file('gmail_token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'gmail_credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('gmail_token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    try:
-        # Call the Gmail API
-        service = build('gmail', 'v1', credentials=creds)
-        results = service.users().labels().list(userId='me').execute()
-        labels = results.get('labels', [])
-
-        if not labels:
-            print('No labels found.')
-            return
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
-
-    except HttpError as error:
-        # TODO(developer) - Handle errors from gmail API.
-        print(f'An error occurred: {error}')
+traverser = imaptraverser.ImapTraverser( imapcredentials )
 
 
-if __name__ == '__main__':
-    main()
+while traverser.nextMessage():
+    logging.info(  f"Processing message {traverser.currentMessageIdx()+1} of {traverser.nrMessagesInFolder()} (UID: {traverser.currentMessageID().id}) in folder {traverser.currentFolderIdx()+1} of {traverser.nrFolders()}: {traverser.currentFolder()}")
+
+
+#        for msgid, data in response.items():
+#            logging.info(f"Pro")
+#            print(
+#                "   ID %d: %d bytes, flags=%s, envelope=%s" % (msgid, data[b"RFC822.SIZE"], data[b"FLAGS"], data[b"ENVELOPE"])
+#            )
+#        return 
